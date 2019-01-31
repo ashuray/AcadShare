83c7b85151533f112cf87934bf6234b786765861
ashuray <akray215@gmail.com>
2019-01-31 10:37:28 +0530

    importing packages

---
+++ app.js
@@ -0,0 +1,14 @@
+var express			 = require("express");
+var methodOverride   = require("method-override");
+var bodyParser       = require("body-parser");
+var mongoose         = require("mongoose");
+var indexRoutes      = require("./routes/index.js");
+
+var app = express();
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/",indexRoutes);
+
+app.listen(3000,'localhost',function(){
+	console.log("server has started");
+});
