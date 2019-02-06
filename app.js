d249cbdca2c15f489d44993245ecc450a24128d0
ashuray <akray215@gmail.com>
2019-02-07 02:07:56 +0530

    Added practice homepage

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var methodOverride   = require("method-override");
 var bodyParser       = require("body-parser");
 var mongoose         = require("mongoose");
 var indexRoutes      = require("./routes/index.js");
+var practiceRoutes      = require("./routes/practice.js");
+var interviewRoutes      = require("./routes/interview.js");
 
 var app = express();
 app.use(express.static('public'))
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/",indexRoutes);
+app.use("/practice",practiceRoutes);
+app.use("/interviews",interviewRoutes);
 
 app.listen(3000,'localhost',function(){
 	console.log("server has started");
